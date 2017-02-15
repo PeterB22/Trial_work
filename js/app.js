6140cd5ab3d30280fa82566a0744bc29cc49345a
PeterB22 <peter.borcsok@szeged.cas.de>
2017-02-15 14:24:04 +0100

    refactoring app js code

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ listCountries.config(function($routeProvider){
            templateUrl : 'pages/edit.html',
            controller : 'EditController'
        })
-
        .otherwise({redirectTo : "/"});
 
 });
@@ -29,24 +28,64 @@ listCountries.controller('HomeController',function($scope){
 });
 
 listCountries.controller('ListController',function($scope,$http){
-    $scope.$watch('search',function(){
-        if(typeof $scope.search != 'undefined') {
-            $http.get("https://restcountries.eu/rest/v1/name/" + $scope.search)
+
+    $scope.$watch('countryName',function() {
+        if (typeof $scope.countryName != 'undefined') {
+
+            $http.get("https://restcountries.eu/rest/v1/name/" + $scope.countryName)
                 .then(function (response) {
                     $scope.details = response.data;
-                    console.log($scope.details);
-                })
+                });
+            if (document.querySelector('[name="searchCountry"]').value == "")
+                $scope.details = undefined;
+
         }
     });
 
-    $scope.update = function(country){
-        $scope.search = country.name;
-    };
+    $scope.$watch('inRegion',function(){
 
-    $scope.select = function(){
-        this.setSelectionRange(0,this.value.length);
-    }
+        $http.get("https://restcountries.eu/rest/v1/region/" + $scope.inRegion)
+            .then(function (response) {
+                $scope.details = response.data;
+            });
+        if(document.querySelector('[name="regions"]').value=="")
+            $scope.details = undefined;
+    });
+
+
+ /*   $scope.$watch('currencyUsedByCountries',function(){
+        if(typeof $scope.currencyUsedByCountries != 'undefined') {
+            $http.get("https://restcountries.eu/rest/v1/currency/" + $scope.currencyUsedByCountries)
+                .then(function (response) {
+                    $scope.details = response.data;
+
+                });
+            if (document.querySelector('[name="searchCurrency"]').value == "")
+                $scope.details = undefined;
+
+        }
+    });*/
+  //  watchElements("currencyUsedByCountries","currency","searchCurrency");
+  //  watchElements("inRegion","region","regions");
+    console.log($scope);
+      watchElements("countryName","name","searchCountry",$scope,$http);
 });
+
+
+function watchElements(target,type,name,scope,http){
+
+    scope.$watch(target,function(){
+        if(typeof scope[target] != 'undefined') {
+            http.get("https://restcountries.eu/rest/v1/" + type + "/"+ scope[target])
+                .then(function (response) {
+                    scope.details = response.data;
+                });
+            if (document.querySelector('[name='+name+']').value == "")
+                scope.details = undefined;
+
+        }
+    })
+}
 
 listCountries.controller('EditController',function($scope){
 
